5be714950d9644d38ddedee9442f59d161e6b7da
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-15 10:42:38 +0200

    add demo code to background.js

---
+++ src/background.js
@@ -0,0 +1,10 @@
+// Code to run when extension gets installed
+chrome.runtime.onInstalled.addListener(function () {
+  // Demo Alert Works!
+  //alert("Chrome Session Quicksave installed successfully");
+  //
+  // Demo Code:
+  // chrome.storage.sync.set({color: '#3aa757'}, function() {
+  //   console.log("The color is green.");
+  // });
+});
