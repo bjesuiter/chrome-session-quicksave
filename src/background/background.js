e9214d48447e30abfbd015f1936d3c619abfe57a
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-17 11:42:57 +0200

    save current tabs to bookmarks

--- src/background/background.js
+++ src/background/background.js
@@ -1,46 +1,50 @@
-import format from "../../node_modules/date-fns/format/index.js";
-import { searchBookmarkFolders, saveSession } from "./bookmark-service";
+import format from '../../node_modules/date-fns/format/index.js';
+import {searchBookmarkFolders, saveSession} from './bookmark-service';
+import {getTabsInWindow} from './tabs-service.js';
 
 // Code to run when extension gets installed
 chrome.runtime.onInstalled.addListener(function () {
-  // Demo Alert Works!
-  //alert("Chrome Session Quicksave installed successfully");
-  //
-  // Demo Code:
-  // chrome.storage.sync.set({color: '#3aa757'}, function() {
-  //   console.log("The color is green.");
-  // });
+	// Demo Alert Works!
+	//alert("Chrome Session Quicksave installed successfully");
+	//
+	// Demo Code:
+	// chrome.storage.sync.set({color: '#3aa757'}, function() {
+	//   console.log("The color is green.");
+	// });
 });
 
 chrome.browserAction.onClicked.addListener(
-  /**
-   * @param currentTab see https://developer.chrome.com/extensions/tabs#type-Tab
-   */
-  async (currentTab) => {
-    console.log("Extension Icon Clicked!");
+	/**
+	 * @param currentTab see https://developer.chrome.com/extensions/tabs#type-Tab
+	 */
+	async currentTab => {
+		const currentWindowId = currentTab.windowId;
 
-    const sessionName = prompt(
-      "Please insert a name for the session",
-      "New Session " + format(new Date(), "yyyy-MM-dd")
-    );
+		const sessionName = prompt(
+			'Please insert a name for the session',
+			'New Session ' + format(new Date(), 'yyyy-MM-dd')
+		);
 
-    // this uses the first result, may break easily!
-    // replace with target folder selection via plugin later
-    const [sessionsFolder] = await searchBookmarkFolders("Sessions");
+		// this uses the first result, may break easily!
+		// replace with target folder selection via plugin later
+		const [sessionsFolder] = await searchBookmarkFolders('Sessions');
+		console.log('SessionsFolder: ', sessionsFolder);
 
-    console.log("SessionsFolder: ", sessionsFolder);
+		const tabs = await getTabsInWindow(currentWindowId);
+		console.log('Tabs', tabs);
 
-    // TODO: saveSession
-    const sessionFolder = await saveSession(sessionsFolder.id, sessionName);
+		// TODO: saveSession
+		const newSessionFolder = await saveSession(sessionsFolder.id, sessionName, tabs);
+		console.log('NewSessionFolder', newSessionFolder);
 
-    // executeScript docs: https://developer.chrome.com/extensions/tabs#method-executeScript
-    // injects modal.js into the current web page to show ionic component boxes
-    //
-    // Note: This feature is non-essential, and poentially hurts the performance of the page
-    // only for showing a fancy ionic promt instead of an extension popup html or js prompt
-    // Therefore it's deactivated currently.
-    // chrome.tabs.executeScript(currentTab.id, {
-    //   file: "src/modal/modal.js",
-    // });
-  }
+		// executeScript docs: https://developer.chrome.com/extensions/tabs#method-executeScript
+		// injects modal.js into the current web page to show ionic component boxes
+		//
+		// Note: This feature is non-essential, and poentially hurts the performance of the page
+		// only for showing a fancy ionic promt instead of an extension popup html or js prompt
+		// Therefore it's deactivated currently.
+		// chrome.tabs.executeScript(currentTab.id, {
+		//   file: "src/modal/modal.js",
+		// });
+	}
 );
--- src/background/bookmark-service.js
+++ src/background/bookmark-service.js
@@ -8,9 +8,9 @@
  * @returns {Promise<chrome.bookmarks.BookmarkTreeNodes>}
  */
 export async function getBookmarkNodes(...bookmarkIds) {
-  return new Promise((resolve) => {
-    chrome.bookmarks.get(bookmarkIds, resolve);
-  });
+	return new Promise(resolve => {
+		chrome.bookmarks.get(bookmarkIds, resolve);
+	});
 }
 
 /**
@@ -19,25 +19,25 @@ export async function getBookmarkNodes(...bookmarkIds) {
  * @returns {Promise<chrome.bookmarks.BookmarkTreeNode>} a single BookmarkTreeNode
  */
 export async function getBookmarkNode(bookmarkId) {
-  return new Promise((resolve) => {
-    chrome.bookmarks.get(bookmarkId, resolve);
-  });
+	return new Promise(resolve => {
+		chrome.bookmarks.get(bookmarkId, resolve);
+	});
 }
 
 /**
  * @returns {Promise<chrome.bookmarks.BookmarkTreeNode>}
  */
 export async function getBookmarkRoot() {
-  return getBookmarkNode("0");
+	return getBookmarkNode('0');
 }
 
 /**
  * @returns {Promise<Array<chrome.bookmarks.BookmarkTreeNodes>>}resolves to array of all available BookmarkTreeNodes
  */
 export async function getBookmarkTreeComplete() {
-  return new Promise((resolve) => {
-    chrome.bookmarks.getTree(resolve);
-  });
+	return new Promise(resolve => {
+		chrome.bookmarks.getTree(resolve);
+	});
 }
 
 /**
@@ -47,15 +47,15 @@ export async function getBookmarkTreeComplete() {
  * @returns array of BookmarkTreeNodes for folders
  */
 export async function searchBookmarkFolders(folderName) {
-  return new Promise((resolve) => {
-    chrome.bookmarks.search(
-      {
-        url: null,
-        title: folderName,
-      },
-      resolve
-    );
-  });
+	return new Promise(resolve => {
+		chrome.bookmarks.search(
+			{
+				url: null,
+				title: folderName
+			},
+			resolve
+		);
+	});
 }
 
 /**
@@ -65,12 +65,32 @@ export async function searchBookmarkFolders(folderName) {
  * @returns Promise, which resolves with the created BookmarkTreeNode for the folder
  */
 export async function createBookmarkFolder(parentId, folderName) {
-  return new Promise((resolve) => {
-    chrome.bookmarks.create({ parentId, title: folderName, url: null }, resolve);
-  });
+	return new Promise(resolve => {
+		chrome.bookmarks.create({parentId, title: folderName, url: null}, resolve);
+	});
 }
 
-export async function createBookmarks() {}
+/**
+ *
+ * @param {*} parentId
+ * @param {*} title
+ * @param {*} url
+ * @returns {Promise<chrome.bookmarks.BookmarkTreeNode>}
+ */
+export async function createBookmark(parentId, url, title) {
+	return new Promise(resolve => {
+		chrome.bookmarks.create({parentId, title, url}, resolve);
+	});
+}
+
+/**
+ *
+ * @param {Array<chrome.tabs.Tab>} tabs
+ */
+export async function createBookmarksForTabs(parentId, tabs) {
+	const bookmarkPromises = tabs.map(tab => createBookmark(parentId, tab.url, tab.title));
+	return Promise.all(bookmarkPromises);
+}
 
 /**
  * Combines several simpler bookmark manipulation functions to save a bunch of tabs as a session
@@ -80,13 +100,15 @@ export async function createBookmarks() {}
  * @param {*} options
  */
 export async function saveSession(parentId, sessionName, tabs, options) {
-  const optionDefaults = { mode: "overwrite" };
-  options = { ...optionDefaults, ...options };
-  const { mode } = options;
+	const optionDefaults = {mode: 'overwrite'};
+	options = {...optionDefaults, ...options};
+	const {mode} = options;
 
-  // create new Folder for session
-  // TODO: Check if folder is already available & which mode is set (overwrite, error, ...)
-  const sessionFolder = await createBookmarkFolder(parentId, sessionName);
+	// create new Folder for session
+	// TODO: Check if folder is already available & which mode is set (overwrite, error, ...)
+	const sessionFolder = await createBookmarkFolder(parentId, sessionName);
 
-  return sessionFolder;
+	await createBookmarksForTabs(sessionFolder.id, tabs);
+
+	return sessionFolder;
 }
