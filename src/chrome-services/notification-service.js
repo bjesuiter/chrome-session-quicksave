555b8a3d816d5e8e2dc1165425c325d25c9cb702
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-17 21:03:31 +0200

    add showError to notification service

--- src/chrome-services/notification-service.js
+++ src/chrome-services/notification-service.js
@@ -1,3 +1,15 @@
+async function basicNotification(title, message, iconUrl) {
+	return new Promise(resolve => {
+		const notification = {
+			type: 'basic',
+			iconUrl: iconUrl,
+			title,
+			message
+		};
+		chrome.notifications.create(null, notification, resolve);
+	});
+}
+
 /**
  *
  * Chrome Docs for NotificationOptions
@@ -5,13 +17,9 @@
  * @returns {Promise<string>} resolves with the id of the notification
  */
 export async function showSimpleNotification(title, message) {
-	return new Promise(resolve => {
-		const notification = {
-			type: 'basic',
-			iconUrl: chrome.extension.getURL('assets/img/icon-512.png'),
-			title,
-			message
-		};
-		chrome.notifications.create(null, notification, resolve);
-	});
+	return basicNotification(title, message, chrome.extension.getURL('assets/img/icon-512.png'));
+}
+
+export async function showError(title, message) {
+	return basicNotification(title, message, chrome.extension.getURL('assets/icons/warning.png'));
 }
