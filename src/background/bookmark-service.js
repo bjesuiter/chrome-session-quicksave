ec24be81d06cffc9dfd0a9d88e910707aa66d15a
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-17 09:13:42 +0200

    improve bookmark service with methods to get specific bookmark nodes

---
+++ src/background/bookmark-service.js
@@ -0,0 +1,41 @@
+/**
+ * https://developer.chrome.com/extensions/bookmarks
+ */
+const folderToStoreNewSessions = "Sessions";
+
+/**
+ *
+ * @param  {...any} bookmarkIds
+ * @returns array of BookmarkTreeNodes
+ */
+export async function getBookmarkNodes(...bookmarkIds) {
+  return new Promise((resolve) => {
+    chrome.bookmarks.get(bookmarkIds, (resultNodes) => resolve(resultNodes));
+  });
+}
+
+/**
+ * An alias function for getBookmarkNodes which returns a single value instead of an array
+ * @param {string} bookmarkId
+ * @returns {bookmarkTreeNodes} a single BookmarkTreeNode
+ */
+export async function getBookmarkNode(bookmarkId) {
+  return new Promise((resolve) => {
+    chrome.bookmarks.get(bookmarkId, (resultNode) => resolve(resultNode));
+  });
+}
+
+export async function getBookmarkRoot() {
+  return getBookmarkNode("0");
+}
+
+/**
+ * @returns Promise
+ */
+export async function getBookmarkTreeComplete() {
+  return new Promise((resolve) => {
+    chrome.bookmarks.getTree((bookmarkTreeNodes) => {
+      resolve(bookmarkTreeNodes);
+    });
+  });
+}
