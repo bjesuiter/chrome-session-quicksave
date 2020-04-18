cf2b75d8cd5d538e7c92f525f4d323947f6e04b0
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-18 20:13:18 +0200

    refactor background.js to typescript

--- src/background.ts
+++ src/background.ts
@@ -4,9 +4,8 @@ import {getTabsInWindow} from './chrome-services/tabs-service';
 import {showSimpleNotification, showError} from './chrome-services/notification-service';
 
 // Code to run when extension gets installed
-chrome.runtime.onInstalled.addListener(function () {
-	// Demo Alert Works!
-	//alert("Chrome Session Quicksave installed successfully");
+chrome.runtime.onInstalled.addListener(function() {
+	// Add code to run after install
 	//
 	// Demo Code:
 	// chrome.storage.sync.set({color: '#3aa757'}, function() {
@@ -14,8 +13,8 @@ chrome.runtime.onInstalled.addListener(function () {
 	// });
 });
 
-async function quicksaveSession(currentTab) {
-	const currentWindowId = currentTab.windowId;
+async function quicksaveSession(currentTab: chrome.tabs.Tab): Promise<void> {
+	const currentWindowId: number = currentTab.windowId;
 
 	const sessionName = prompt(
 		'Please insert a name for the session',
@@ -29,15 +28,15 @@ async function quicksaveSession(currentTab) {
 
 	// this uses the first result, may break easily!
 	// replace with target folder selection via plugin later
-	const [sessionsFolder] = await searchBookmarkFolders('Sessions');
-	console.log('SessionsFolder: ', sessionsFolder);
-
-	const tabs = await getTabsInWindow(currentWindowId);
-	console.log('Tabs', tabs);
-
+	const [sessionsFolder]: chrome.bookmarks.BookmarkTreeNode[] = await searchBookmarkFolders('Sessions');
+	const tabs: chrome.tabs.Tab[] = await getTabsInWindow(currentWindowId);
 	const newSessionFolder = await saveSession(sessionsFolder.id, sessionName, tabs);
-	console.log('NewSessionFolder', newSessionFolder);
-	return newSessionFolder;
+
+	// Todo: show notification which allows jumping to the new session folder in bookmark manager view
+	await showSimpleNotification(
+		'Session Quicksave - Success',
+		`The new Session "${sessionName}" was saved successfully`
+	);
 }
 
 chrome.browserAction.onClicked.addListener(
@@ -46,12 +45,7 @@ chrome.browserAction.onClicked.addListener(
 	 */
 	async currentTab => {
 		try {
-			const sessionFolder = await quicksaveSession(currentTab);
-			// Todo: show notification which allows jumping to the new session folder in bookmark manager view
-			await showSimpleNotification(
-				'Session Quicksave - Success',
-				`The new Session "${sessionFolder.title}" was saved successfully`
-			);
+			await quicksaveSession(currentTab);
 		} catch (error) {
 			await showError('Session Quicksave - Error', `The session could not be saved.`);
 			console.error(error);
