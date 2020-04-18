d6930b4d9cddd6a700cebcefe627a82c69246169
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-18 19:23:25 +0200

    install rollup typescript integration

--- rollup.config.js
+++ rollup.config.js
@@ -5,24 +5,26 @@ import commonjs from '@rollup/plugin-commonjs';
 import {chromeExtension} from 'rollup-plugin-chrome-extension';
 import copy from 'rollup-plugin-copy2';
 import zip from 'rollup-plugin-zip';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
 	input: 'src/manifest.json',
 	output: {
 		dir: 'build',
-		format: 'esm',
+		format: 'esm'
 	},
 	plugins: [
 		// always put chromeExtension() before other plugins
 		chromeExtension(),
+		typescript(),
 		// the plugins below are optional
 		resolve(),
 		commonjs(),
 		copy({
-			assets: ['Readme.md', 'Changelog.md'],
+			assets: ['Readme.md', 'Changelog.md']
 		}),
 		zip({
-			dir: 'dist',
-		}),
-	],
+			dir: 'dist'
+		})
+	]
 };
