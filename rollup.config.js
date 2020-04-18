6c25986e3847393e06f438836a20dbe9e0b6d7bb
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-18 20:38:39 +0200

    fix typescript compile

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,10 @@
 // import { rollup } from "rollup";
 
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import {chromeExtension} from 'rollup-plugin-chrome-extension';
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
 import copy from 'rollup-plugin-copy2';
 import zip from 'rollup-plugin-zip';
-import typescript from '@rollup/plugin-typescript';
 
 export default {
 	input: 'src/manifest.json',
@@ -17,9 +16,7 @@ export default {
 		// always put chromeExtension() before other plugins
 		chromeExtension(),
 		typescript(),
-		// the plugins below are optional
 		resolve(),
-		commonjs(),
 		copy({
 			assets: ['Readme.md', 'Changelog.md']
 		}),
