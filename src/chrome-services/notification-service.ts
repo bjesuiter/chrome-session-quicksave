60f01bbc67fe2b035c56a13ef6924a5c45146721
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-18 19:24:07 +0200

    convert notification-service to typescript by adding types

--- src/chrome-services/notification-service.ts
+++ src/chrome-services/notification-service.ts
@@ -1,4 +1,4 @@
-async function basicNotification(title, message, iconUrl) {
+async function basicNotification(title: string, message: string, iconUrl: string) {
 	return new Promise(resolve => {
 		const notification = {
 			type: 'basic',
@@ -6,7 +6,7 @@ async function basicNotification(title, message, iconUrl) {
 			title,
 			message
 		};
-		chrome.notifications.create(null, notification, resolve);
+		chrome.notifications.create(notification, resolve);
 	});
 }
 
@@ -16,10 +16,10 @@ async function basicNotification(title, message, iconUrl) {
  * @param {*} text
  * @returns {Promise<string>} resolves with the id of the notification
  */
-export async function showSimpleNotification(title, message) {
+export async function showSimpleNotification(title: string, message: string) {
 	return basicNotification(title, message, chrome.extension.getURL('assets/img/icon-512.png'));
 }
 
-export async function showError(title, message) {
+export async function showError(title: string, message: string) {
 	return basicNotification(title, message, chrome.extension.getURL('assets/icons/error.png'));
 }
