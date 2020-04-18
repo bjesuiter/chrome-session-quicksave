b915f24c524034eba3e4896e2843d1110f0056b3
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-18 22:08:03 +0200

    add emptyDir rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,8 @@ import {chromeExtension} from 'rollup-plugin-chrome-extension';
 import postcss from 'rollup-plugin-postcss';
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
+// https://www.npmjs.com/package/rollup-plugin-empty-dir
+import {emptyDir} from 'rollup-plugin-empty-dir';
 import copy from 'rollup-plugin-copy2';
 import zip from 'rollup-plugin-zip';
 
@@ -23,6 +25,7 @@ export default {
 		}),
 		typescript(),
 		resolve(),
+		emptyDir(),
 		copy({
 			assets: ['Readme.md', 'Changelog.md']
 		}),
