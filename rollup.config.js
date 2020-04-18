5d768f9d783e80f568c7c2343ece0d33332e10d1
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-18 17:03:16 +0200

    remove unused pushReloader (because it throws errors when including)

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,20 @@
 // import { rollup } from "rollup";
 
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import { chromeExtension, pushReloader } from "rollup-plugin-chrome-extension";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import {chromeExtension} from 'rollup-plugin-chrome-extension';
 
 export default {
-  input: "src/manifest.json",
-  output: {
-    dir: "dist",
-    format: "esm",
-  },
-  plugins: [
-    // always put chromeExtension() before other plugins
-    chromeExtension(),
-    pushReloader(),
-    // the plugins below are optional
-    resolve(),
-    commonjs(),
-  ],
+	input: 'src/manifest.json',
+	output: {
+		dir: 'dist',
+		format: 'esm',
+	},
+	plugins: [
+		// always put chromeExtension() before other plugins
+		chromeExtension(),
+		// the plugins below are optional
+		resolve(),
+		commonjs(),
+	],
 };
