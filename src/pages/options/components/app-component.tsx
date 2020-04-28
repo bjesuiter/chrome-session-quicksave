ac5cee4c23e903396b94b850c5bb262c7bcb223e
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2020-04-28 20:51:40 +0200

    try to use material-ui/core in a chrome extension

--- src/pages/options/components/app-component.tsx
+++ src/pages/options/components/app-component.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import {Button} from '@material-ui/core';
 
 export default function App(props: any) {
-	return <h1>My custom React App Component!</h1>;
+	return (
+		<div>
+			<h1>Session Quicksave - Options</h1>
+			<label htmlFor="defaultSessionsFolderInput">Default Sessions Folder:</label>
+			<input type="text" id="defaultSessionsFolderInput" />
+
+{/* This button doesn't work right now, but JSX in general works */}
+			<Button color="primary">Material Core Button - Save</Button>
+		</div>
+	);
 }
