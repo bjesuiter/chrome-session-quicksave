4a372d19950c9786ed6e390130e7ac433747792d
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-08 10:08:57 +0200

    add all other stencil resources

--- src/assets/chrome/background.ts
+++ src/assets/chrome/background.ts
@@ -1,16 +1,18 @@
 import format from 'date-fns/format';
-import {searchBookmarkFolders, saveSession} from './chrome-services/bookmark-service';
-import {getTabsInWindow} from './chrome-services/tabs-service';
-import {showSimpleNotification, showError} from './chrome-services/notification-service';
+import {searchBookmarkFolders, saveSession} from '../../chrome-services/bookmark-service';
+import {getTabsInWindow} from '../../chrome-services/tabs-service';
+import {showSimpleNotification, showError} from '../../chrome-services/notification-service';
 
 // Code to run when extension gets installed
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
 	// Add code to run after install
 	//
 	// Demo Code:
 	// chrome.storage.sync.set({color: '#3aa757'}, function() {
 	//   console.log("The color is green.");
 	// });
+
+	alert('Extension Installed!');
 });
 
 async function quicksaveSession(currentTab: chrome.tabs.Tab): Promise<void> {
