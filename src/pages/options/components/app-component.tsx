9ad794bef4d5e04c9395e2f7f4405f444085d80b
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-08 11:03:03 +0200

    deactivate old react app-component

--- src/pages/options/components/app-component.tsx
+++ src/pages/options/components/app-component.tsx
@@ -1,48 +1,48 @@
-import React, {useState} from 'react';
-import {getBookmarkTreeComplete} from '@chrome/bookmark-service';
+// import React, {useState} from 'react';
+// import {getBookmarkTreeComplete} from '@chrome/bookmark-service';
 
-function renderListNodes(list: chrome.bookmarks.BookmarkTreeNode[]) {
-	return list.map(node => (
-		<li>
-			<h3>{node.title}</h3>
-			<ul>{node.children ? renderListNodes(node.children) : undefined}</ul>
-		</li>
-	));
-}
+// function renderListNodes(list: chrome.bookmarks.BookmarkTreeNode[]) {
+// 	return list.map(node => (
+// 		<li>
+// 			<h3>{node.title}</h3>
+// 			<ul>{node.children ? renderListNodes(node.children) : undefined}</ul>
+// 		</li>
+// 	));
+// }
 
-export default function App(props: any) {
-	const [bookmarks, setBookmarks] = useState<Array<chrome.bookmarks.BookmarkTreeNode>>([]);
-	getBookmarkTreeComplete().then(bookmarks => setBookmarks(bookmarks));
+// export default function App(props: any) {
+// 	const [bookmarks, setBookmarks] = useState<Array<chrome.bookmarks.BookmarkTreeNode>>([]);
+// 	getBookmarkTreeComplete().then(bookmarks => setBookmarks(bookmarks));
 
-	return (
-		<div>
-			<h1>Session Quicksave - Options</h1>
-			<label htmlFor="defaultSessionsFolderInput">Default Sessions Folder:</label>
-			<input type="text" id="defaultSessionsFolderInput" />
-			<hr />
-			<h1>Demo Area</h1>
-			{/* <button ion-button>Button</button> */}
-			{/* <IonButton>Default</IonButton> */}
-			{/* <h2>Bookmark List</h2> */}
-			{/* <ul>
-				{renderListNodes(bookmarks)}
-			</ul> */}
-		</div>
-	);
+// 	return (
+// 		<div>
+// 			<h1>Session Quicksave - Options</h1>
+// 			<label htmlFor="defaultSessionsFolderInput">Default Sessions Folder:</label>
+// 			<input type="text" id="defaultSessionsFolderInput" />
+// 			<hr />
+// 			<h1>Demo Area</h1>
+// 			{/* <button ion-button>Button</button> */}
+// 			{/* <IonButton>Default</IonButton> */}
+// 			{/* <h2>Bookmark List</h2> */}
+// 			{/* <ul>
+// 				{renderListNodes(bookmarks)}
+// 			</ul> */}
+// 		</div>
+// 	);
 
-	//  Favorite color:
-	// <select id="color">
-	//     <option value="red">red</option>
-	//     <option value="green">green</option>
-	//     <option value="blue">blue</option>
-	//     <option value="yellow">yellow</option>
-	// </select>
+// 	//  Favorite color:
+// 	// <select id="color">
+// 	//     <option value="red">red</option>
+// 	//     <option value="green">green</option>
+// 	//     <option value="blue">blue</option>
+// 	//     <option value="yellow">yellow</option>
+// 	// </select>
 
-	// <label>
-	//     <input type="checkbox" id="like">
-	//     I like colors.
-	// </label>
+// 	// <label>
+// 	//     <input type="checkbox" id="like">
+// 	//     I like colors.
+// 	// </label>
 
-	// <div id="status"></div>
-	// <button id="save">Save</button></label>
-}
+// 	// <div id="status"></div>
+// 	// <button id="save">Save</button></label>
+// }
