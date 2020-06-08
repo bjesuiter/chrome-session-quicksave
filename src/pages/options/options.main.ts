a4c7ef2785be5f67309e53ff7bba76bcc4d8c201
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-06-08 11:58:52 +0200

    restructure option page

--- src/pages/options/options.main.ts
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from './components/app-component';
-
-const reactRoot = document.querySelector('#react-root');
-ReactDOM.render(React.createElement(App), reactRoot);
