16016a5ad0b363f90630f69a63a1b66652178989
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-11-12 21:49:52 +0100

    add functions to save and read options from synced chrome json storage

---
+++ src/lib/chrome-services/synced-storage-service.ts
@@ -0,0 +1,28 @@
+import {deserialize, serialize} from 'serializr';
+import {SessionQuicksaveOptions, SessionQuicksaveOptionsModel} from 'src/models/session-quicksave-options';
+
+export function saveOptions(options: SessionQuicksaveOptions): Promise<void> {
+	return new Promise((resolve, reject) => {
+		const jsonObject = serialize(options);
+		chrome.storage.sync.set(jsonObject, () => {
+			if (chrome.runtime.lastError) {
+				reject(chrome.runtime.lastError);
+			} else {
+				resolve();
+			}
+		});
+	});
+}
+
+export function readOptions(): Promise<SessionQuicksaveOptions> {
+	return new Promise((resolve, reject) => {
+		chrome.storage.sync.get(items => {
+			if (chrome.runtime.lastError) {
+				reject(chrome.runtime.lastError);
+			} else {
+				const decodedOptions = deserialize(SessionQuicksaveOptionsModel, items);
+				resolve(decodedOptions);
+			}
+		});
+	});
+}
