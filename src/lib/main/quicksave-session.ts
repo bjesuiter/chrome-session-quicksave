df6e3474f13b144811c3df112bf80a24b48be10f
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-11-12 22:51:09 +0100

    fix check for prompt cancellation

--- src/lib/main/quicksave-session.ts
+++ src/lib/main/quicksave-session.ts
@@ -2,9 +2,21 @@ import format from 'date-fns/format';
 import { searchBookmarkFolders, saveSession } from '@lib/chrome-services/bookmark-service';
 import { getTabsInWindow } from '@lib/chrome-services/tabs-service';
 import { showSimpleNotification } from '@lib/chrome-services/notification-service';
+import { readOptions, saveOptions } from '../chrome-services/synced-storage-service';
+import { SessionQuicksaveOptions } from '@models/session-quicksave-options';
 
+/**
+ * This function will be called by src/global/app.ts when extension icon is clicked
+ * @param currentTab
+ */
 export async function quicksaveSession(currentTab: chrome.tabs.Tab): Promise<void> {
 	const currentWindowId: number = currentTab.windowId;
+
+	// Test storage functionality
+	await saveOptions(new SessionQuicksaveOptions('mySessionsFolder123456'));
+	const result = await readOptions();
+
+	console.log('SessionsFolder ID: ', result.sessionsFolderId);
 
 	const sessionName = prompt(
 		'Please insert a name for the session',
@@ -12,7 +24,7 @@ export async function quicksaveSession(currentTab: chrome.tabs.Tab): Promise<voi
 	);
 
 	// user clicked on 'cancel' for the session name promt
-	if (sessionName === undefined || sessionName.length < 0) {
+	if (!sessionName || sessionName.length < 0) {
 		return;
 	}
 
