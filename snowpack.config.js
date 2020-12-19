8e5577a60255bc03275a5ef1909c8d747216d9fe
Benjamin Jesuiter <bjesuiter@gmail.com>
2020-12-19 13:32:38 +0100

    add new project structure in projects folder

---
+++ snowpack.config.js
@@ -0,0 +1,26 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/#configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+	mount: {
+		'webroot': { url: '/', static: true },
+		'projects/background-script': { url: '/' },
+		'projects/options-page/webroot': { url: '/', static: true },
+		'projects/options-page/src': { url: '/dist' },
+	},
+	plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-typescript'],
+	install: [],
+	installOptions: {},
+	devOptions: {
+		fallback: 'options.html',
+	},
+	buildOptions: {
+		metaDir: 'private/snowpack',
+	},
+	proxy: {},
+	alias: {
+		'@lib': './projects/lib',
+		'@options-page': './projects/options-page/src',
+	},
+};
